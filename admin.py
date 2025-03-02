e777ca09a81f20b98c353e63857c2a6acace463a
Rohan-Bhati <166688799+ROHAN2027@users.noreply.github.com>
2025-03-02 16:25:09 +0530

    Delete admin.py

--- admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from info.models import info
-
-class infoadmin(admin.ModelAdmin):
-    list_display = ('name', 'email')
-
-admin.site.register(info, infoadmin)
-
-# Register your models here.
